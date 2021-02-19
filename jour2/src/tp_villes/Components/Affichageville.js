96489f682cdd0d371403cb0adb105f1002eef3a7
nicklas <nsenentz@hotmail.fr>
2021-02-19 10:23:21 +0100

    fin tp-villes & new-arbo

--- jour2/src/tp_villes/Components/Affichageville.js
+++ jour2/src/tp_villes/Components/Affichageville.js
@@ -1,15 +1,18 @@
+import { useState } from "react";
 
 
 function Affichageville(props) {
 
-
+    const [ modifier, usemodifier] = useState(false)
+    
     return (
         <div>
            
             <h1>{props.pNameville}</h1>
             <h3>{props.pNhabitant}</h3>
             <h3>{props.pMonville}</h3>
-            <button>Modifier</button>
+            {!modifier && <button onClick ={() => {usemodifier(true); props.pHandleModifier(props.pid)}}>Modifier</button>}
+            {modifier && <button onClick ={() =>{ usemodifier(false);props.pHandleModifierComp(props.pid)}}>Valider</button>}
         </div>
     )
 }
--- jour2/src/tp_villes/Components/Inputdata.js
+++ jour2/src/tp_villes/Components/Inputdata.js
@@ -18,6 +18,8 @@ class Inputcpn extends React.Component {
         this.HandlechangeHbts = this.HandlechangeHbts.bind(this)
         this.HandlechangeMonument = this.HandlechangeMonument.bind(this)
         this.HandleAjouter = this.HandleAjouter.bind(this)
+        this.HandleModifier = this.HandleModifier.bind(this)
+        this.HandleModifierComp = this.HandleModifierComp.bind(this)
     }
     HandlechangeVille(e) {
         this.setState({ Nameville: e.target.value })
@@ -42,29 +44,53 @@ class Inputcpn extends React.Component {
                 ListVille: copy
             })
         }
+    }
 
-        
+
+    HandleModifier(index) {
+        var copie = this.state.ListVille
+        this.setState({ Nameville: copie[index].Nomville })
+        this.setState({ Nhabitant: copie[index].Nhabitant })
+        this.setState({ Monville: copie[index].Monville })
+    }
+
+    HandleModifierComp(index){
+        var copie = this.state.ListVille
+        copie[index].Nameville = this.state.Nameville
+        copie[index].Nhabitant = this.state.Nhabitant
+        copie[index].Monville = this.state.Monville
+        this.setState({
+            Nameville: '',
+            Nhabitant: 0,
+            Monville: '',
+            ListVille: copie
+        })
     }
 
     render() {
         return (
             <div>
-               <div className='Inputsdata'>
-               <h1>COMPOSANT </h1>
-                <input type="text" name="Nameville" placeholder="Nom de la ville "
-                    onChange={this.HandlechangeVille} value={this.state.Nomville}></input>
-                <input type="number" name="Nhabitant" placeholder="Nb habitants "
-                    onChange={this.HandlechangeHbts} value={this.state.Nhabitant}></input>
-                <input type="text" name="Monville" placeholder="Monument de la ville "
-                    onChange={this.HandlechangeMonument} value={this.state.Monville}></input>
-                <button onClick={this.HandleAjouter}>Add</button>
-               </div>
-           <div>
-           {this.state.ListVille.map((iville, index)=>{
-                  return  <Affichageville pNameville={iville.Nameville} 
-                  pNhabitant={iville.Nhabitant} pMonville={iville.Monville}
-                  />})}
-           </div>
+                <div className='Inputsdata'>
+                    <h1>COMPOSANT </h1>
+                    <input type="text" name="Nameville" placeholder="Nom de la ville "
+                        onChange={this.HandlechangeVille} value={this.state.Nomville}></input>
+                    <input type="number" name="Nhabitant" placeholder="Nb habitants "
+                        onChange={this.HandlechangeHbts} value={this.state.Nhabitant}></input>
+                    <input type="text" name="Monville" placeholder="Monument de la ville "
+                        onChange={this.HandlechangeMonument} value={this.state.Monville}></input>
+                    <button onClick={this.HandleAjouter}>Add</button>
+                </div>
+                <div>
+                    {this.state.ListVille.map((iville, index) => {
+                        return <Affichageville key={index}
+                            pNameville={iville.Nameville}
+                            pNhabitant={iville.Nhabitant} pMonville={iville.Monville}
+                            pHandleModifier={this.HandleModifier}
+                            pHandleModifierComp={this.HandleModifierComp}
+                            pid={index}
+                        />
+                    })}
+                </div>
             </div>
         )
     }
